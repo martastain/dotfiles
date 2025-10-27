782ae69d0e0b4f784363f80e0bff4902e7e10bee
Martastain <martas@imm.cz>
2025-10-27 18:21:57 +0100

    fix: treesitter autoload

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,73 +1,50 @@
--- Treesitter configuration
-
-
-local function deduplicate(list)
-  local seen = {}
-  local result = {}
-  for _, item in ipairs(list) do
-    if not seen[item] then
-      seen[item] = true
-      table.insert(result, item)
-    end
-  end
-  return result
-end
-
 return {
-  "nvim-treesitter/nvim-treesitter",
+  'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdate",
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  keys = {
-    { "<c-space>", desc = "Increment Selection" },
-    { "<bs>", desc = "Decrement Selection", mode = "x" },
-  },
-  opts_extend = { "ensure_installed" },
-  config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      opts.ensure_installed = deduplicate(opts.ensure_installed)
-    end
-    require("nvim-treesitter.configs").setup(opts)
-  end,
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    ensure_installed = {
-      "bash",
-      "c",
-      "diff",
-      "html",
-      "javascript",
-      "jinja",
-      "jinja2",
-      "jsdoc",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "printf",
-      "python",
-      "query",
-      "regex",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
+  config = function()
+    local configs = require 'nvim-treesitter.configs'
+    configs.setup {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "htmldjango",
+        "javascript",
+        "jinja",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+        'elixir',
       },
-    },
-  },
+      highlight = {
+        enable = true
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    }
+  end
 }
-
